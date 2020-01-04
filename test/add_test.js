79d29cbfbd39ce26e0ee448df13b3b2016223d69
HannaChung <hanna.chung@titansoft.com.sg>
2020-01-04 22:29:10 +0800

    modify add js file name

--- test/add_test.js
+++ test/add_test.js
@@ -1,9 +1,9 @@
-import add from '../index';
+import add from '../add';
 
 describe('Testing in add function', ()=> {
     it('Test case : 10 + 10 = 20', ()=> {
         if (add(10, 10) !== 20) {
-            throw new Error("Test fail!");
+            throw new Error("Test fail of add!");
         }
     });
 });
