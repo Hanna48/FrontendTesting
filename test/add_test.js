a63c69e162424132a62572d28e22ad44a160e8f2
HannaChung <hanna.chung@titansoft.com.sg>
2020-01-05 17:47:13 +0800

    add assert example with Chai

--- test/add_test.js
+++ test/add_test.js
@@ -1,11 +1,14 @@
 import add from '../add';
 import chai from 'chai';
 const expect = chai.expect;
+//const assert = chai.assert;
+
 //var expect = require('chai').expect;
 //const add = require('../add');
 
 describe('Testing in add function', ()=> {
     it('Test case : 10 + 10 = 20', ()=> {
         expect(add(10, 10)).to.be.equal(20);
+        //assert(add(10, 10) === 20, 'not equal');
     });
 });
